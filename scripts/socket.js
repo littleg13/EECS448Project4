3ab5804c74d0c4f52af91bc9319076e55c4001ef
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2019-03-27 19:05:26 -0500

    Missed some files in the last push

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,8 +1,35 @@
 let socket = io('https://448.cuzzo.net');
-function connect() {
-  socket.emit('joinLobby', {id : 'lobbyCode'})
+
+function joinLobby() {
+  lobbyCode = document.getElementById('lobbyCode').value;
+  name = document.getElementById('username').value;
+  socket.emit('joinLobby', {lobbyCode : lobbyCode, username : name});
+};
+
+function createLobby() {
+  name = document.getElementById('username').value;
+  socket.emit('createLobby', {username : name});
 };
 
 socket.on('message', function (data) {
     console.log(data);
-  });
+});
+
+socket.on('lobbyCreated', function (data) {
+    console.log(data);
+    
+});
+
+socket.on('setID', function (data) {
+  localStorage.setItem('userID', data['userID']);
+});
+
+socket.on('connect', function (data) {
+  if(localStorage.userID){
+    socket.emit('auth', {userID : localStorage.userID});
+  }
+});
+
+socket.on('error', function (data) {
+  console.log(data['error']);
+});
